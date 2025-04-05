ec11b1d545786584a2d22a13ce34d0eaa21619f5
harsh27-afk <itisharsh27@gmail.com>
2025-04-05 10:09:42 +0530

    clerk auth implemented

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import {  Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { Toaster } from "sonner";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter=Inter({subsets:['latin']})
 
@@ -12,6 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${inter.className}`}
@@ -26,5 +28,6 @@ export default function RootLayout({ children }) {
           </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
