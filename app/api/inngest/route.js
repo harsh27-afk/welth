456988295166dd60140ad962269480385a4706de
harsh27-afk <itisharsh27@gmail.com>
2025-04-20 18:21:32 +0530

    Created Inngest client and connection established

---
+++ app/api/inngest/route.js
@@ -0,0 +1,16 @@
+import { serve } from "inngest/next";
+
+import { inngest } from "@/lib/inngest/client";
+import {
+  checkBudgetAlerts,
+  generateMonthlyReports,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from "@/lib/inngest/function";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+   
+  ],
+});
